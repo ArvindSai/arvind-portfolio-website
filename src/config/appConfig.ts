f8ea39f86cf6b5e2242652f31d122c97501724ea
Arvind Venkat Sai Vummadisingu <arvindsaiv@Arvinds-MacBook-Pro.local>
2025-04-08 01:33:18 -0400

    refactor: move the site nav items to app config

---
+++ src/config/appConfig.ts
@@ -0,0 +1,46 @@
+export const appConfig = {
+  siteNavOptions: [
+    {
+      label: "Blog",
+      href: "blog",
+    },
+    {
+      label: "Projects",
+      href: "projects",
+    },
+    {
+      label: "Github",
+      href: "github",
+    },
+  ],
+  blogArticles: [
+    {
+      title: "Build your own lyrics (song generation)",
+      description:
+        "Create your own tunes per your taste that you always wanted to build.",
+      image: "blog-img-1.webp",
+      slug: "build-your-ai-beat",
+    },
+    {
+      title: "NextBigTide.net",
+      description:
+        "Get alerts on the next big breakthrough stock and track their returns",
+      image: "blog-img-1.webp",
+      slug: "next-break-through-stock",
+    },
+    {
+      title: "CompanyAncestry.com",
+      description:
+        "Identify the ancestors of a young company based on where the employees worked in the past",
+      image: "blog-img-1.webp",
+      slug: "company-ancestry",
+    },
+    {
+      title: "TectonicShifts.unveil",
+      description:
+        "Observe the changes in the countries due to changing immigration",
+      image: "blog-img-1.webp",
+      slug: "tectonic-shift",
+    },
+  ],
+};
