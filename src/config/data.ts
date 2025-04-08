e47ce9dcfddb11b0faac4a8daf4d40bc3560b4c8
Arvind Venkat Sai Vummadisingu <arvindsaiv@Arvinds-MacBook-Pro.local>
2025-04-08 00:29:37 -0400

    ui feat: mapped the blog articles with config data

--- src/config/data.ts
+++ src/config/data.ts
@@ -3,25 +3,25 @@ export const appConfig = {
         {
             title: "Build your own lyrics (song generation)",
             description: "Create your own tunes per your taste that you always wanted to build.",
-            image: "/arvind-portfolio-website/build-a-beat",
+            image: "blog-img-1.webp",
             slug: "build-your-ai-beat"
         },
         {
             title: "NextBigTide.net",
             description: "Get alerts on the next big breakthrough stock and track their returns",
-            image: "/arvind-portfolio-website/build-a-beat",
+            image: "blog-img-1.webp",
             slug: "next-break-through-stock"
         },
         {
             title: "CompanyAncestry.com",
             description: "Identify the ancestors of a young company based on where the employees worked in the past",
-            image: "/arvind-portfolio-website/build-a-beat",
+            image: "blog-img-1.webp",
             slug: "company-ancestry"
         },
         {
             title: "TectonicShifts.unveil",
             description: "Observe the changes in the countries due to changing immigration",
-            image: "/arvind-portfolio-website/build-a-beat",
+            image: "blog-img-1.webp",
             slug: "tectonic-shift"
         },
     ]
